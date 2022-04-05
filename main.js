94030f959b8751304717bc6c356ff48a00f2865d
sarumon9 <60652742+sarumon9@users.noreply.github.com>
2022-04-05 14:47:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $('.slider').slick({
     autoplay:true,
-    // infinite:true,
+    infinite:true,
     slidesToShow:1,
-    // slidesToScroll:3,
+    slidesToScroll:1,
     prevArrow:'<div class="slick-prev"></div>',
     nextArrow:'<div class="slick-next"></div>',
     dots:true
